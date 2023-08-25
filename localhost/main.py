08c32c7ba8d2d66322e6fce5f79d430b4ed49a5e
Siddhant Purohit <siddhant.0519@gmail.com>
2023-08-25 14:08:16 -0700

    Merge gpt and Pyneapple backend

--- localhost/main.py
+++ localhost/main.py
@@ -4,12 +4,13 @@ import os
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, project_root)
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, File, UploadFile
 from read_shapefile import read_shapefile
 from Pyneapple.pyneapple.weight.rook import from_dataframe as rook
 from Pyneapple.pyneapple.regionalization.expressive_maxp import expressive_maxp
-from Pyneapple.pyneapple.regionalization.MaxP import maxp
+from Pyneapple.pyneapple.regionalization.maxp import maxp
 from Pyneapple.pyneapple.regionalization.generalized_p import generalized_p
+from Pyneapple.pyneapple.regionalization.scalable_maxp import scalable_maxp
 import uvicorn
 import json
 from typing import Dict, Union
@@ -24,21 +25,27 @@ from typing import List
 from pydantic import BaseModel
 import math
 import numpy as np
+from libpysal.weights import Queen, Rook
+from spopt.region.maxp import maxp as libMaxP
+import time
+
 
 app = FastAPI()
 
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allows CORS from this origin
+    allow_origins=["http://localhost:3000", "http://localhost:3001"],  # Allows CORS from this origin
     allow_credentials=True,
     allow_methods=["*"],  # Allows all HTTP methods
     allow_headers=["*"],
 )
 
+# GPT-Backend
+
 openai.api_key = os.getenv('OPENAI_API_KEY')
 # print(openai.api_key)
-data_dir = "D:/user_pa1n/VSCode/projects/GPT-Pyneapple/testData"
+data_dir = "../testData"
 
 
 # class Label(BaseModel):
@@ -222,7 +229,7 @@ def gpt_process_query(user_query: str, file_name: str):
 
     chat_history = []
     chat_history.append({"role": "system","content": "Only choose from the functions provided to you. Default values for lower bound parameters is negative infinty and upper bound is infintiy. Only use ',' as a delimeter for larger integers"})
-    chat_history.append({"role": "system", "content": "Function generalized_p is chosen if the number of regions to partition the dataset is mentioned by the user. If the nu,ber of regions/partitions are not mentioned you should choose the function maxp"})
+    chat_history.append({"role": "system", "content": "Function generalized_p is chosen if the number of regions to partition the dataset is mentioned by the user. If the number of regions/partitions are not mentioned you should choose the function maxp"})
     chat_history.append({"role": "system", "content": "Make sure to choose the required string parameters for each function."})
     # chat_history.append({"role": "user","content": "Based on the name of the columns of the  dataframe help select appropiate column as arguments for functions based on the user query. Give a detailed analysis of inference of each column name"})
     # chat_history.append({"role": "user", "content": "The columns in the dataframe are : " + df_context})
@@ -335,24 +342,6 @@ def gpt_process_query(user_query: str, file_name: str):
     return gptResult
 
 
-@app.get("/files/{filename}")
-async def read_file_fe(filename: str):
-    gdf = read_shapefile(data_dir, filename)
-    gdf = gdf[['geometry']]
-    gdf['id'] = range(1, len(gdf) + 1)
-    # print(gdf)
-    return json.loads(gdf.to_json())
-
-
-@app.get("/listFiles")
-async def list_files():
-    files = []
-    for filename in os.listdir(data_dir):
-        if filename.endswith(".shp"):
-            files.append(filename)
-    return files
-
-
 @app.post("/api/endpoint")
 def GPTmaxPEndPoint(parameters: Dict[str, Union[float, str]], filename: str):
 
@@ -436,6 +425,161 @@ def gpEndPoint(parameters: Dict[str, Union[int, float, str]], filename: str):
 
     return jsonPrucResult
 #print(pruc(gdf, w, 'PCGDP1940', 'PERIMETER', 3000000, 10))
+
+
+## OverLapping end points
+
+@app.get("/listFiles")
+async def list_files():
+    files = []
+    for filename in os.listdir(data_dir):
+        if filename.endswith(".shp"):
+            files.append(filename)
+    return files
+
+
+@app.get("/listWeights")
+async def list_weights():
+    weights = ['Queen', 'Rook']
+    return weights 
+
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    file_name = file.filename
+    file_path = os.path.join(data_dir, file_name)
+
+    with open(file_path, "wb") as f:
+        contents = await file.read()
+        f.write(contents)
+
+    return {"Successfully uploaded : ", file_name}
+
+
+@app.get("/files/{filename}")
+async def read_file_fe(filename: str):
+    gdf = read_shapefile(data_dir, filename)
+    gdf = gdf[['geometry']]
+    gdf['id'] = range(1, len(gdf) + 1)
+    # print(gdf)
+    return json.loads(gdf.to_json())
+
+# Pyneapple Backend
+
+
+@app.get("/dfDetails")
+async def df_details(filename):
+
+    df = read_shapefile(data_dir, filename)
+    numeric_cols = df.select_dtypes(include=['number']).columns
+    available_cols = [(col, df[col].min().item(), df[col].max().item()) for col in numeric_cols]
+    print("Available_cols: ", available_cols)
+    return available_cols
+
+
+@app.get("/api/endpoint/emp")
+def empEndPoint(file_name: str, weight: str, disname: str,
+                minName: str, minLow: float, minHigh: float,
+                maxName: str, maxLow: float, maxHigh: float,
+                avgName: str, avgLow: float, avgHigh: float,
+                sumName: str, sumLow: float, sumHigh: float,
+                countLow: float, countHigh: float):
+
+    df = read_shapefile(data_dir, file_name)
+    if weight == 'Rook':
+        w = Rook.from_dataframe(df)
+    elif weight == 'Queen':
+        w = Queen.from_dataframe(df)
+    else:
+        print("Invalid weight selected")
+
+    empStartTime = time.time()
+    max_p, labels = expressive_maxp(df, w, disname, minName, minLow, minHigh,
+                                    maxName, maxLow, maxHigh, avgName, avgLow, avgHigh,
+                                    sumName, sumLow, sumHigh, countLow, countHigh)
+    empEndTime = time.time()
+    empExecTime = empEndTime - empStartTime
+    labels = labels.tolist()
+    empResult = {"execution_time": empExecTime, "max_p": max_p, "labels": labels}
+
+    return empResult
+
+
+@app.get("/api/endpoint/generalizedP")
+def gmpEndPoint(file_name: str, weight: str, sim_attr: str, ext_attr: str,
+                threshold: float, p: int):
+
+    df = read_shapefile(data_dir, file_name)
+    if weight == 'Rook':
+        w = Rook.from_dataframe(df)
+    elif weight == 'Queen':
+        w = Queen.from_dataframe(df)
+    else:
+        print("Invalid weight selected")
+    prucStartTime = time.time()
+    prucLabels = generalized_p(df, w, sim_attr, ext_attr, threshold, p)
+    prucEndTime = time.time()
+    prucExecTime = prucEndTime - prucStartTime
+    prucResult = {"execution_time": prucExecTime, "labels": prucLabels}
+
+    return prucResult
+
+
+@app.get("/api/endpoint/libraryMaxP")
+def libraryMaxP(file_name: str, weight: str, attr_name: str, threshold_name: str, threshold: float):
+
+    df = read_shapefile(data_dir, file_name)
+    if weight == 'Rook':
+        w = Rook.from_dataframe(df)
+    elif weight == 'Queen':
+        w = Queen.from_dataframe(df)
+    else:
+        print("Invalid weight selected")
+    lmpStartTime = time.time()
+    lmpLabels = libMaxP(df, w, attr_name, threshold_name, threshold, 2)
+    lmpEndTime = time.time()
+    lmpExecTime = lmpEndTime - lmpStartTime
+    lmpResult = {"execution_time": lmpExecTime, "labels": lmpLabels}
+
+    return lmpResult
+
+
+@app.get("/api/endpoint/ScalableMaxP")
+def smp(file_name: str, weight: str, sim_attr: str, ext_attr: str, threshold: float):
+
+    df = read_shapefile(data_dir, file_name)
+    # w = Rook.from_dataframe(df)
+    if weight == 'Rook':
+        w = Rook.from_dataframe(df)
+    elif weight == 'Queen':
+        w = Queen.from_dataframe(df)
+    else:
+        print("Invalid weight selected")
+    smpStartTime = time.time()
+    smpLabels = scalable_maxp(df, w, sim_attr, ext_attr, threshold)
+    smpEndTime = time.time()
+    smpExecTime = smpEndTime - smpStartTime
+    smpResult = {"execution_time": smpExecTime, "labels": smpLabels}
+
+    return smpResult
+
+
+@app.get("/api/endpoint/compareMaxP")
+def compareMaxP(file_name: str, weight: str, sim_attr: str, ext_attr: str,
+                threshold: float):
+    
+    lmpResult = libraryMaxP(file_name, weight, sim_attr, ext_attr, threshold)
+    lmpTime = lmpResult["execution_time"]
+    smpResult = smp(file_name, weight, sim_attr, ext_attr, threshold)
+    smpTime = smpResult["execution_time"]
+    speedup = lmpTime/smpTime
+
+    comparisionResult = {"ScalableMaxP_ExecutionTime": smpTime,
+                         "LibraryMaxP_ExecutionTime": lmpTime,
+                         "Total_SpeedUp(Percentage)": speedup*100,
+                         "ScalableMaxP_Labels": smpResult["labels"],
+                         "LibraryMaxP_Labels": lmpResult["labels"]}
+    return comparisionResult
 
 
 if __name__ == "__main__":
